38fdc3742a00bc3c7ea03b85dfb7bd01515e769d
eyysma <asmaznine98@gmail.com>
2022-12-01 11:21:05 +0100

    created brands list in product servie

--- product-service/src/main/java/com/ayoam/productservice/repository/BrandRepository.java
+++ product-service/src/main/java/com/ayoam/productservice/repository/BrandRepository.java
@@ -4,4 +4,5 @@ import com.ayoam.productservice.model.Brand;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BrandRepository extends JpaRepository<Brand,Long> {
+
 }
